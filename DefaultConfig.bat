3128e65fb6c49d5f5f4c9f585ea0923cd5cba075
eppic <ncraft121@gmail.com>
2021-06-05 05:04:49 +0200

    Stores the Default Config

---
+++ DefaultConfig.bat
@@ -0,0 +1,8 @@
+::Name pattern of downloaded files
+set FileNaming=/%%(title)s.%%(ext)s
+::Destination of downloaded files
+set Destination=%userprofile%\Downloads\ytBATCH
+::Open file destination after download
+set DestOpen=F
+::Use Cookies automatically when downloading
+set AutoCookies=F
