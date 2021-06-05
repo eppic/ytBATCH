07fc84c3ad0ec205e17bd7fdd799583f63b3fe9c
eppic <ncraft121@gmail.com>
2021-06-06 00:18:21 +0200

    Optimized Manual Mode

--- bin/ManualMode.bat
+++ bin/ManualMode.bat
@@ -5,19 +5,21 @@ if /I "%mtx%"=="" cls
 ::Manual Mode
     set mmexe=youtube-dl
     set manualcode=
-    :mmplus
     if /I "%mtx%"=="" cls
-    echo Manual Mode - Enter Code by yourself - If not allocated the output is in the root folder of ytBATCH. 
+    
+    echo Manual Mode - Path is root of ytBATCH
     echo - - - - -
     echo (B) Go Back...
-    if /I "%mmexe%"=="youtube-dl" echo (M) Manual Mode++
+    echo (M) Manual Mode++
     echo - - - - -
+
+    :mreturn
     set /p manualcode=%mmexe% 
-    if /I "%manualcode%"=="M" set mmexe= & goto mmplus
+
+    if /I "%manualcode%"=="M" set mmexe= & goto mreturn
     if /I "%manualcode%"=="B" call Launcher.bat
-    if /I "%mtx%"=="" cls
-    echo %mmexe% %manualcode% 
+   
     %mmexe% %manualcode%
-    pause
-    call Launcher.bat
+    echo - - - - -
+    goto mreturn
     
