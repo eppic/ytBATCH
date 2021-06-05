d2d64b9c1140b8ecd00644155e1782dfbd4e381f
eppic <ncraft121@gmail.com>
2021-06-06 01:56:43 +0200

    Update Help.bat

--- bin/Help.bat
+++ bin/Help.bat
@@ -3,6 +3,6 @@ title Help - ytBATCH %version%
 if /I "%mtx%"=="" cls
 
 ::Open youtube-dl Help Page
-youtube-dl --help
-pause
-call MainMenu.bat
+    youtube-dl --help
+    pause
+    call MainMenu.bat
