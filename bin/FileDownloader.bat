11bca3a0354eb98d85ed6971d5660b363e152be1
eppic <ncraft121@gmail.com>
2021-06-06 22:36:44 +0200

    Optimized for Github

--- bin/FileDownloader.bat
+++ bin/FileDownloader.bat
@@ -1,67 +1,12 @@
 @echo off
-title Download Files - ytBATCH %version%
+title Downloading youtube-dl.exe - ytBATCH %version%
 if /I "%mtx%"=="" cls
 
-::Launch Checker
-    if /i "%fdcode%"=="ytdl" goto ytdl_dl
-    if /i "%fdcode%"=="ffmp" goto ffmp_dl
-    if /i "%fdcode%"=="atpa" goto atpa_dl
+echo Wait for youtube-dl.exe to download... 
+echo This will take a moment.
+powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://youtube-dl.org/downloads/latest/youtube-dl.exe', '..\exe\youtube-dl.exe')"
+echo - - - - -
+echo youtube-dl.exe downloaded.
+pause
 
-::File Downloader Menu
-    echo Download Files!
-    echo (Y) Download youtube-dl.exe
-    echo (F) Download ffmpeg.exe
-    echo (A) Download AtomicParsley.exe
-    echo (P) Download ffprobe.exe
-    echo - - - - -
-    echo (B) Go Back...
-
-    set /p DLFilesCode=COMMAND:
-
-    if /i "%DLFilesCode%"=="Y" goto ytdl_dl
-    if /i "%DLFilesCode%"=="F" goto ffmp_dl
-    if /i "%DLFilesCode%"=="A" goto atpa_dl
-    if /i "%DLFilesCode%"=="P" goto ffpr_dl
-    if /i "%DLFilesCode%"=="" call FileDownloader.bat
-
-    if /i "%DLFilesCode%"=="B" call Preferences.bat
-
-    call FileDownloader.bat
-
-::File Downloader
-    :ytdl_dl
-    if /I "%mtx%"=="" cls
-    echo Wait for youtube-dl.exe to download... 
-    echo This will take a moment.
-    powershell -command "(New-Object System.Net.WebClient).DownloadFile(\"https://youtube-dl.org/downloads/latest/youtube-dl.exe\", \"youtube-dl.exe\")"
-    echo - - - - -
-    echo youtube-dl.exe downloaded.
-    pause
-    call Launcher.bat
-
-    :ffmp_dl
-    echo Wait for ffmpeg.exe to download... 
-    echo This will take a moment.
-    powershell -command "(New-Object System.Net.WebClient).DownloadFile(\"https://docs.google.com/uc?export=download&id=1H6Ft1f7fVwyEcbwinFIygLNyuj9a6j22", \"ffmpeg.exe\")"
-    echo - - - - -
-    echo ffmpeg.exe downloaded.
-    pause
-    call Launcher.bat
-
-    :atpa_dl
-    echo Wait for AtomicParsley.exe to download... 
-    echo This will take a moment.
-    powershell -command "(New-Object System.Net.WebClient).DownloadFile(\"https://bit.ly/3t3Grcd\", \"AtomicParsley.exe\")"
-    echo - - - - -
-    echo AtomicParsley.exe downloaded.
-    pause
-    call Launcher.bat
-
-    :ffpr_dl
-    echo Wait for ffprobe.exe to download... 
-    echo This will take a moment.
-    powershell -command "(New-Object System.Net.WebClient).DownloadFile(\"https://bit.ly/3mTiwtI\", \"ffprobe.exe\")"
-    echo - - - - -
-    echo ffprobe.exe downloaded.
-    pause
-    call Launcher.bat
+call Launcher.bat
--- bin/Launcher.bat
+++ bin/Launcher.bat
@@ -7,39 +7,20 @@ if /I "%mtx%"=="" cls
 
     call DefaultConfig.bat
     call UserConfig.bat
+    
     if /I "%mtx%"=="" cls
 
 ::Check Settings
-    if "%AutoCookies%"=="T" set CookieSet=--cookies "cookies.txt"
+    ::if "%AutoCookies%"=="T" set CookieSet=--cookies "cookies.txt"
 
-::File Check
-    if exist "..\exe\youtube-dl.exe" goto filecheck_ytdl
+::Check File Integrity...
+    if exist "..\exe\youtube-dl.exe" goto intcheck_ytdl
     echo youtube-dl.exe not found. 
     echo Press any key to download the latest version of youtube-dl.exe...
     pause>nul
     set fdcode=ytdl
     call FileDownloader.bat
-    :filecheck_ytdl
+    :intcheck_ytdl
 
-    if exist "..\exe\ffmpeg.exe" goto filecheck_ffmpeg
-    echo ffmpeg.exe not found. 
-    echo Press any key to download ffmpeg.exe ...
-    pause>nul
-    set fdcode=ffmp
-    call FileDownloader.bat
-    :filecheck_ffmpeg
-
-    ::Skipping AtomicParsley
-    goto filecheck_atomic
-
-    if exist "..\exe\AtomicParsley.exe" goto filecheck_atomic
-    echo AtomicParsley.exe not found. 
-    echo Press any key to download AtomicParsley.exe ...
-    pause>nul
-    set fdcode=atpa
-    call FileDownloader.bat
-    :filecheck_atomic
-
-    set fdcode=
 ::Open Menu
     call MainMenu.bat
