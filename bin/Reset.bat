e465650b4a2c80fb25087f8ab08b1b4ec20bb439
eppic <ncraft121@gmail.com>
2021-06-07 14:10:10 +0200

    new reset menu

--- bin/Reset.bat
+++ bin/Reset.bat
@@ -3,30 +3,29 @@ title Reset - ytBATCH %version%
 if /I "%mtx%"=="" cls
 
 ::Reset Menu
-echo WARNING!
-echo If you proceed ytBATCH will get deleted and re-downloaded.
-echo - - - - -
-echo [reset123] Proceed...
-echo [B] Go Back...
-echo - - - - -
-
-set /p ResetCode=COMMAND:
-
-if /i "%ResetCode%"=="reset123" goto ResetCheck
-if /i "%ResetCode%"=="B" call Preferences.bat
-
-call Reset.bat
-
-:ResetCheck
-echo Press any key to confirm the deletion of the above listed files.
-pause>nul
-del youtube-dl.exe
-del ffmpeg.exe
-del AtomicParsley.exe
-if exist ffprobe.exe del ffprobe.exe
-if exist UserConfig.bat del UserConfig.bat
-echo - - - - -
-echo Deleted everything.
-pause
-if /I "%mtx%"=="" cls
-call Launcher.bat
+    echo WARNING!
+    echo If you proceed ytBATCH will get deleted and re-downloaded.
+    echo - - - - -
+    echo [reset123] Proceed...
+    echo [B] Go Back...
+    echo - - - - -
+    
+    set /p ResetCode=COMMAND:
+    
+    if /i "%ResetCode%"=="reset123" goto ResetCheck
+    if /i "%ResetCode%"=="B" call Preferences.bat
+    
+    call Reset.bat
+    
+    :ResetCheck
+    pause
+    del /q ..\UserConfig.bat
+    del /q ..\ytBATCH.lnk
+    rmdir /s /q ..\exe
+    start ..\installer.bat
+    rmdir /s /q ..\bin
+    
+    echo ERROR: Something went wrong. 
+    pause 
+    call New.bat
+    
