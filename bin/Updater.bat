a808ff4cd893b5c65b5b8b56d7be8eeeb6ced91a
eppic <ncraft121@gmail.com>
2021-06-13 18:46:45 +0200

    Updater is now in Beta

--- bin/Updater.bat
+++ bin/Updater.bat
@@ -2,8 +2,42 @@
 title Checking for Updates - ytBATCH %version%
 %mcls%
 
-::Check for yt-dl Updates
-echo Please Wait...
-..\exe\youtube-dl -U
-pause
-call MainMenu.bat
+
+::Update Menu
+    set UpdateCode=
+    echo d= ytdl; t= ytbatch-main; b= back
+    set /p UpdateCode=COMMAND:
+    if /I "%UpdateCode%"=="T" goto ytbatch-main-dl
+    if /I "%UpdateCode%"=="D" goto ytdl-dl
+    if /I "%UpdateCode%"=="B" call MainMenu.bat
+    
+    call Updater.bat
+
+::youtube-dl Updater
+    :ytdl-dl
+    echo Please Wait...
+    ..\exe\youtube-dl -U
+    pause
+    call MainMenu.bat
+
+::ytBATCH Updater
+    :ytbatch-main-dl
+    ::download
+    powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/eppic/ytBATCH/archive/refs/heads/main.zip', '..\ytb_temp.zip')"
+    pause
+    
+    ::extract
+    powershell -command "(Expand-Archive -Force ..\ytb_temp.zip ..\ )"
+    pause
+    
+    ::move
+    xcopy ..\ytBATCH-main\* ..\ /E /I /H /Y
+    pause
+    
+    ::delete
+    rmdir ..\ytBATCH-main /S /Q
+    pause
+    del ..\ytb_temp.zip /Q
+    pause
+    
+    call MainMenu.bat
