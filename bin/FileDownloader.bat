3a696596661a7d8ae98c446c24b2429cf71d3b87
eppic <ncraft121@gmail.com>
2021-06-13 18:46:20 +0200

    moved ytb update to updater

--- bin/FileDownloader.bat
+++ bin/FileDownloader.bat
@@ -1,6 +1,3 @@
-goto atpdl
-
-
 @echo off
 title Downloading youtube-dl.exe - ytBATCH %version%
 %mcls%
@@ -14,30 +11,3 @@ echo youtube-dl.exe downloaded.
 pause
 
 call Launcher.bat
-
-
-:atpdl
-::download
-powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/eppic/ytBATCH/archive/refs/heads/main.zip', '..\ytb_temp.zip')"
-pause
-
-::extract
-powershell -command "(Expand-Archive -Force ..\ytb_temp.zip ..\ )"
-pause
-
-::move
-xcopy ..\ytBATCH-main\* ..\ /E /I /H /Y
-pause
-
-::delete
-rmdir ..\ytBATCH-main /S /Q
-pause
-del ..\ytb_temp.zip /Q
-
-
-echo fin
-pause
-exit
-
-https://github.com/eppic/ytBATCH/archive/refs/heads/main.zip
-
