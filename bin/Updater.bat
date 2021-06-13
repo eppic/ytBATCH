4cd371b8baa35d5e6852def4de90df2c29d6c02a
eppic <ncraft121@gmail.com>
2021-06-13 18:58:05 +0200

    fixed

--- bin/Updater.bat
+++ bin/Updater.bat
@@ -2,7 +2,6 @@
 title Checking for Updates - ytBATCH %version%
 %mcls%
 
-
 ::Update Menu
     set UpdateCode=
     echo d= ytdl; t= ytbatch-main; b= back
@@ -14,30 +13,35 @@ title Checking for Updates - ytBATCH %version%
     call Updater.bat
 
 ::youtube-dl Updater
-    :ytdl-dl
+    :ytdl-
+    echo - - - - -
     echo Please Wait...
     ..\exe\youtube-dl -U
+    echo - - - - -
     pause
     call MainMenu.bat
 
 ::ytBATCH Updater
     :ytbatch-main-dl
+    echo - - - - -
+
     ::download
+    echo Downloading latest Main Branch...
     powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/eppic/ytBATCH/archive/refs/heads/main.zip', '..\ytb_temp.zip')"
-    pause
-    
+
     ::extract
+    echo Extracting zip...
     powershell -command "(Expand-Archive -Force ..\ytb_temp.zip ..\ )"
-    pause
-    
+
     ::move
+    echo Moving...
     xcopy ..\ytBATCH-main\* ..\ /E /I /H /Y
-    pause
-    
+
     ::delete
+    echo Deleting temporary files...
     rmdir ..\ytBATCH-main /S /Q
-    pause
     del ..\ytb_temp.zip /Q
+    echo - - - - -
+    echo Updated ytBATCH to latest Main Branch (Problems may occur!)
     pause
-    
-    call MainMenu.bat
+    call New.bat
