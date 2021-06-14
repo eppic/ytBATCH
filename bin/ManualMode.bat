3946e775348058969d9d1ad57dd58dd807eaf9f2
eppic <ncraft121@gmail.com>
2021-06-14 10:38:14 +0200

    rm mmpp because c option

--- bin/ManualMode.bat
+++ bin/ManualMode.bat
@@ -9,34 +9,14 @@ title Manual Mode - ytBATCH %version%
     echo Manual Mode - Path is root of ytBATCH
     echo - - - - -
     echo (B) Go Back...
-    echo (M) Manual Mode++
     echo - - - - -
 
     :mmreturn
     set /p manualcode=youtube-dl 
 
-    if /I "%manualcode%"=="M" goto mmplus
     if /I "%manualcode%"=="B" call Launcher.bat
    
     ..\exe\youtube-dl.exe %manualcode%
 
     goto mmreturn
-    
-    :mmplus
-
-    %mcls%
-    echo Manual Mode ++ - no prefix
-    echo - - - - -
-    echo (B) Go Back...
-    echo - - - - -
-
-    :mmpreturn
-    cd ..\exe\
-    set /p manualpluscode=
-    
-    if /I "%manualpluscode%"=="B" cd ..\bin\ & call Launcher.bat 
-
-    %manualpluscode%
-
-    goto mmpreturn
     
