50fc146871c03d32a0fdb809e93c37ac1dc13c28
eppic <ncraft121@gmail.com>
2021-06-26 14:05:50 +0200

    Added Playlist Support, fixed Cookies Option

--- bin/UrlHandler.bat
+++ bin/UrlHandler.bat
@@ -2,9 +2,10 @@
 title Enter Action - ytBATCH %version%
 %mcls%
 
-::Cookie Settings
+::Cookie/Playlist Settings
     if "%AutoCookies%"=="T" (set CookieSet=--cookies "..\cookies.txt") else (set CookieSet=)
-    :CookiesPassed
+    set PlaylistSet=--no-playlist
+    :UrlHPassed
     %mcls%
 
 ::Url Handler
@@ -16,8 +17,12 @@ title Enter Action - ytBATCH %version%
     echo   (V) Video
     echo   (F) List All Formats...
     echo.
-    if "%CookieSet%"=="--cookies "..\cookies.txt"" echo   (C) Cookies activated!
-    if not "%CookieSet%"=="--cookies "..\cookies.txt"" echo   (C) Use Cookies...
+
+    if "%PlaylistSet%"=="--yes-playlist" echo   (P) Playlist (TRUE)
+    if "%PlaylistSet%"=="--no-playlist" echo   (P) Playlist (FALSE)
+    if "%CookieSet%"=="--cookies "..\cookies.txt"" echo   (C) Cookies (TRUE)
+    if not "%CookieSet%"=="--cookies "..\cookies.txt"" echo   (C) Cookies (FALSE)
+
     echo.
     echo (B) Go Back...
     echo.
@@ -25,21 +30,29 @@ title Enter Action - ytBATCH %version%
     set /p UrlCode=COMMAND:
     
     ::Set Download Options
-       set DlOpt=-o "%Destination%%FileNaming%" %CookieSet% --add-metadata --config-location ..\custom.conf 
+       set DlOpt=-o "%Destination%%FileNaming%" %CookieSet% %PlaylistSet% --add-metadata --config-location ..\custom.conf 
 
     if /i "%UrlCode%"=="A" call AudioDL.bat
     if /i "%UrlCode%"=="V" call VideoDL.bat
     if /i "%UrlCode%"=="F" call ListDL.bat
     if /i "%UrlCode%"=="C" goto CookieSetHandler
+    if /i "%UrlCode%"=="P" goto PlSetHandler
     if /i "%UrlCode%"=="B" call MainMenu.bat
 
     call UrlHandler.bat
 
+::Playlist Handler
+    :PlSetHandler
+    if "%PlaylistSet%"=="--yes-playlist" set PlaylistSet=--no-playlist&goto UrlHPassed
+    set PlaylistSet=--yes-playlist
+    goto UrlHPassed
+    
 ::Cookie Handler
     :CookieSetHandler
     if not exist ..\cookies.txt goto NoCookies
+    if "%CookieSet%"=="--cookies "..\cookies.txt"" set CookieSet=&goto UrlHPassed
     set CookieSet=--cookies "..\cookies.txt"
-    goto CookiesPassed
+    goto UrlHPassed
     
     :NoCookies
     %mcls%
