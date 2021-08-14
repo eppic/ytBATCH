1388f360b8923274c0a3ab80961e306da46e7b9b
eppic <ncraft121@gmail.com>
2021-08-14 20:45:32 +0200

    lnk -> powershell

---
+++ bin/makeshortcut.ps1
@@ -0,0 +1,9 @@
+$YtbLink = $env:APPDATA + "\ytBATCH\ytBATCH.bat"
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\ytBATCH.lnk")
+$Shortcut.TargetPath = "$YtbLink"
+$YtbWD = $env:APPDATA + "\ytBATCH\"
+$Shortcut.WorkingDirectory = "$YtbWD"
+$YtbIL = $env:APPDATA + "\ytBATCH\bin\256.ico"
+$Shortcut.IconLocation = "$YtbIL"
+$Shortcut.Save()
