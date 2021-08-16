04987c18fdf17262abc906b51583c2032abf81fd
eppic <ncraft121@gmail.com>
2021-08-16 14:26:54 +0200

    added start menu lnk

--- bin/makeshortcut.ps1
+++ bin/makeshortcut.ps1
@@ -1,6 +1,17 @@
 $YtbLink = $env:APPDATA + "\ytBATCH\ytBATCH.bat"
+$YtbLinkSM = $env:APPDATA + "\Microsoft\Windows\Start Menu\Programs\ytBATCH.lnk"
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\ytBATCH.lnk")
+
+$Shortcut.TargetPath = "$YtbLink"
+$YtbWD = $env:APPDATA + "\ytBATCH\"
+$Shortcut.WorkingDirectory = "$YtbWD"
+$YtbIL = $env:APPDATA + "\ytBATCH\bin\256.ico"
+$Shortcut.IconLocation = "$YtbIL"
+$Shortcut.Save()
+
+$Shortcut = $WshShell.CreateShortcut("$YtbLinkSM")
+
 $Shortcut.TargetPath = "$YtbLink"
 $YtbWD = $env:APPDATA + "\ytBATCH\"
 $Shortcut.WorkingDirectory = "$YtbWD"
