051a9a9a51f805d8bb9e6a9b8c9139b0205790ae
eppic <ncraft121@gmail.com>
2021-08-20 17:36:00 +0200

    overhaul: custom.conf to yt-dl.conf, yt-dlp check

--- bin/Launcher.bat
+++ bin/Launcher.bat
@@ -2,28 +2,40 @@
 title ytBATCH %version%
 %mcls%
 
-::Set Config
+::Load Config
     set Version=2.6.3
-    
-    call DefaultConfig.bat
-    call ..\cfg\UserConfig.bat
-    
-    if /i "%mecho%"=="" (set mcls=cls) else (set mcls=echo -)
 
-::Check Config
+    ::Check for Config, open Installer 
     if not exist ..\cfg\UserConfig.bat call Installer.bat
 
-::Clean Queue
+    ::load config, overwrite with user
+    call DefaultConfig.bat
+    call ..\cfg\UserConfig.bat
+
+    ::rename custom.conf to youtube-dl.conf, create new one
+    if exist "..\cfg\custom.conf" ren "..\cfg\custom.conf" "youtube-dl.conf"
+    if not exist ..\cfg\youtube-dl.conf copy nul ..\cfg\youtube-dl.conf
+            
+    ::echo off switch ("echo")
+    if /i "%mecho%"=="" (set mcls=cls) else (set mcls=echo -)
+
+    ::Clean Queue
     if "%CleanQueueStart%"=="T" del ..\QueueList.bat
     %mcls%
 
+    ::old version yt-dl check
+    if not exist ..\exe\youtube-dl.exe goto oldvcheckpassed
+    if exist ..\exe\yt-dlp.exe goto oldvcheckpassed
+    echo ytBATCH has switched from youtube-dl.exe to yt-dlp.exe
+    echo This requires the latest version of yt-dlp.
+    pause
+    %mcls%
+    :oldvcheckpassed
+    
 ::Check File Integrity...
-    if not exist "..\exe\youtube-dl.exe" set fdcode=ytdl& echo youtube-dl.exe not found. & echo Press any key to download the latest version of youtube-dl.exe... & pause>nul & call FileDownloader.bat
-    set ytdl=
+    if not exist "..\exe\yt-dlp.exe" set fdcode=ytdlp& echo yt-dlp.exe not found. & echo Press any key to download the latest version of yt-dlp.exe... & pause>nul & call FileDownloader.bat
     if not exist "..\exe\ffmpeg.exe" set fdcode=ffmp& echo ffmpeg.exe not found. & echo Press any key to download the latest version of ffmpeg.exe... & pause>nul & call FileDownloader.bat
-    set ffmp=
     if not exist "..\exe\AtomicParsley.exe" set fdcode=atps& echo AtomicParsley.exe not found. & echo Press any key to download the latest version of AtomicParsley.exe... & pause>nul & call FileDownloader.bat
-    set atps=
 
 ::Open Menu
     call MainMenu.bat
