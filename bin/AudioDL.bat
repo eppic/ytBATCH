52874671cb5957727376c86d2093465d3f42bc24
eppic <ncraft121@gmail.com>
2021-09-08 14:42:37 +0200

    MP3 and MP4 -> (M), Thumbnail Check Option

--- bin/AudioDL.bat
+++ bin/AudioDL.bat
@@ -9,7 +9,7 @@ title Download Audio - ytBATCH %version%
 
     echo   (S) Source (Best/No Formatting)
     echo   (W) .WAV
-    echo   (3) .MP3
+    echo   (M) .MP3
     echo   (4) .M4A
     echo   (F) .FLAC
     echo.
@@ -21,6 +21,7 @@ title Download Audio - ytBATCH %version%
     if /i "%AudioDLCode%"=="S" set AudioDLFormat=best & goto AudioDLCheck
     if /i "%AudioDLCode%"=="W" set AudioDLFormat=wav & goto AudioDLCheck
     if /i "%AudioDLCode%"=="3" set AudioDLFormat=mp3 & goto AudioDLCheck
+    if /i "%AudioDLCode%"=="M" set AudioDLFormat=mp3 & goto AudioDLCheck
     if /i "%AudioDLCode%"=="4" set AudioDLFormat=m4a & goto AudioDLCheck
     if /i "%AudioDLCode%"=="F" set AudioDLFormat=flac & goto AudioDLCheck
     if /i "%AudioDLCode%"=="B" call UrlHandler
@@ -30,14 +31,18 @@ title Download Audio - ytBATCH %version%
 ::Add Url to History 
     if "%HstEnb%"=="T" echo %Url%>> ..\History.txt
 
+::Embed Thumbnail Check
+    if "%EmbThb%"=="T" set EmbThbPref=--embed-thumbnail
+
+
 ::Add to Queue
-    if "%QueueSet%"=="T" echo ..\exe\%ytdlv% -x --audio-format %AudioDLFormat% %%DlOpt%% --embed-thumbnail %Url% >> ..\QueueList.bat & call MainMenu.bat
+    if "%QueueSet%"=="T" echo ..\exe\%ytdlv% -x --audio-format %AudioDLFormat% %%DlOpt%% %EmbThbPref% %Url% >> ..\QueueList.bat & call MainMenu.bat
 
 ::Download Audio
     %mcls%
     echo Audio
 
-    ..\exe\%ytdlv% -x --audio-format %AudioDLFormat% %DlOpt% --embed-thumbnail %Url% 
+    ..\exe\%ytdlv% -x --audio-format %AudioDLFormat% %DlOpt% %EmbThbPref% %Url% 
     if "%DestOpen%"=="T" explorer %Destination% 
 
     echo.
