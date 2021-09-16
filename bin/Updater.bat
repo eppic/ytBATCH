fe46d6fbd495547dd513f369324feade7298e207
eppic <ncraft121@gmail.com>
2021-09-16 19:47:34 +0200

    auto update

--- bin/Updater.bat
+++ bin/Updater.bat
@@ -10,6 +10,8 @@ title Check for Updates - ytBATCH %version%
     echo   (Y) Open latest ytBATCH release...
     echo   (D) Update %ytdlv%...
     echo.
+    echo   (G) ytbatch update attempt (cur: %version%; uc: %latestversion%)
+    echo.
     echo   (T) Update to latest Main Branch (experimental)
     echo.
     echo (B) Go Back...
@@ -20,6 +22,7 @@ title Check for Updates - ytBATCH %version%
     if /i "%UpdateCode%"=="Y" start "" "https://github.com/eppic/ytBATCH/releases/latest"
     if /i "%UpdateCode%"=="T" goto ytbatch-main-dl
     if /i "%UpdateCode%"=="D" goto ytdl-dl
+    if /i "%UpdateCode%"=="G" goto ytbatch-release-dl
     if /i "%UpdateCode%"=="B" call MainMenu.bat
     
     call Updater.bat
@@ -34,7 +37,7 @@ title Check for Updates - ytBATCH %version%
     pause
     call MainMenu.bat
 
-::ytBATCH Updater
+::ytBATCH Updater (Main)
     :ytbatch-main-dl
     set ytbupcode=
     %mcls%
@@ -59,6 +62,48 @@ title Check for Updates - ytBATCH %version%
     echo Downloading latest Main Branch...
     powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/eppic/ytBATCH/archive/refs/heads/main.zip', '..\ytb_temp.zip')"
 
+    ::extract
+    echo Extracting zip...
+    powershell -command "(Expand-Archive -Force ..\ytb_temp.zip ..\ )"
+
+    ::move
+    echo Moving...
+    xcopy ..\ytBATCH-main\* ..\ /E /i /H /Y
+
+    ::delete
+    echo Deleting temporary files...
+    rmdir ..\ytBATCH-main /S /Q
+    del ..\ytb_temp.zip /Q
+    echo.
+    echo Updated ytBATCH to latest Main Branch (Problems may occur!)
+    pause
+    call Launcher.bat
+
+::ytBATCH Updater (Release)
+    :ytbatch-release-dl
+    powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/eppic/ytBATCH/main/bin/VersionNumber.info', '..\updatecheck_temp.info')"
+    
+    ::set latest version to var
+    set /p latestversion=<..\updatecheck_temp.info
+    del ..\updatecheck_temp.info
+    
+    ::check if error
+    if /i "%latestversion%"=="" echo ERROR & pause & call Updater.bat
+
+    ::check if same
+    if /i "%version%"=="%latestversion%" (goto UpMsg_UpToDate) else (goto UpMsg_UpdateDL)
+        
+    :UpMsg_UpToDate
+    echo latest
+    pause
+    call Updater.bat
+
+    :UpMsg_UpdateDL
+    echo update!
+    pause
+    
+    powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/eppic/ytBATCH/releases/download/2.7/ytBATCH_%latestversion%.zip', '..\ytb_temp.zip')"
+    
     ::extract
     echo Extracting zip...
     powershell -command "(Expand-Archive -Force ..\ytb_temp.zip ..\ )"
