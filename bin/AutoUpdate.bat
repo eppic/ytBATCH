8d212c727f907de1357159c8fe7d51084ffd22d3
eppic <ncraft121@gmail.com>
2021-10-14 12:30:02 +0200

    auto updater

---
+++ bin/AutoUpdate.bat
@@ -0,0 +1,25 @@
+@echo off
+title Auto Updater
+%mcls%
+
+::ytBATCH
+    :ytbatch-release-dl
+    powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/eppic/ytBATCH/main/bin/versionnumber.info', '..\updatecheck_temp.info')"
+    
+    ::set latest version to var
+    set /p latestversion=<..\updatecheck_temp.info
+    del ..\updatecheck_temp.info
+    
+    ::check if error
+    if /i "%latestversion%"=="" exit
+
+    ::check if same
+    if /i "%version%"=="%latestversion%" (goto UpMsg_UpToDate) else (goto UpMsg_UpdateDL)
+        
+    :UpMsg_UpToDate
+    exit
+
+    :UpMsg_UpdateDL
+    copy nul ..\update.info
+    echo %latestversion% > ..\update.info
+    goto ytBATCH_AU_Check
