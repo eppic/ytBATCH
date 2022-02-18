43bcc1b6eefe9d340257cbfd1336aed670369306
eppic <ncraft121@gmail.com>
2022-02-18 15:01:53 +0100

    rm old autoupdate system

--- bin/AutoUpdate.ps1
+++
@@ -1,9 +0,0 @@
-$AULink = (Get-Item .).FullName + "\AutoUpdate.bat"
-$WshShell = New-Object -comObject WScript.Shell
-
-$AUSc = (Get-Item .).FullName + "\AutoUpdate.lnk"
-$Shortcut = $WshShell.CreateShortcut("$AUSc")
-
-$Shortcut.TargetPath = "$AULink"
-$Shortcut.WindowStyle = 7
-$Shortcut.Save()
